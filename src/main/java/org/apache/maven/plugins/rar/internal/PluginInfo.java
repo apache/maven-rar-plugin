adf575023372561724b2b920a17f70b30c89d872
Tamas Cservenak <tamas@cservenak.net>
2022-07-12 16:18:04 +0200

    [MRAR-87] Upgrade to Maven 3.2.5 and drop legacy

---
+++ src/main/java/org/apache/maven/plugins/rar/internal/PluginInfo.java
@@ -0,0 +1,83 @@
+package org.apache.maven.plugins.rar.internal;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Plugin info helper.
+ */
+public final class PluginInfo
+{
+    private final String groupId;
+
+    private final String artifactId;
+
+    private final String version;
+
+    public PluginInfo( String groupId, String artifactId, String version )
+    {
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.version = version;
+    }
+
+    public String getGroupId()
+    {
+        return groupId;
+    }
+
+    public String getArtifactId()
+    {
+        return artifactId;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+    public static PluginInfo get()
+    {
+        return PLUGIN_INFO;
+    }
+
+    private static final PluginInfo PLUGIN_INFO;
+
+    static
+    {
+        Properties properties = new Properties();
+        try ( InputStream inputStream = PluginInfo.class.getResourceAsStream( "pluginInfo.properties" ) )
+        {
+            properties.load( inputStream );
+        }
+        catch ( IOException e )
+        {
+            // what here?
+        }
+        PLUGIN_INFO = new PluginInfo(
+                properties.getProperty( "groupId", "unknown" ),
+                properties.getProperty( "artifactId", "unknown" ),
+                properties.getProperty( "version", "unknown" )
+        );
+    }
+}
---
+++ src/main/java/org/apache/maven/plugins/rar/internal/RarLifecycleMappingProvider.java
@@ -0,0 +1,104 @@
+package org.apache.maven.plugins.rar.internal;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import javax.inject.Named;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.maven.lifecycle.mapping.Lifecycle;
+import org.apache.maven.lifecycle.mapping.LifecycleMapping;
+
+/**
+ * {@code rar} packaging plugins bindings provider for {@code default} lifecycle.
+ */
+@Named( "rar" )
+@Singleton
+public final class RarLifecycleMappingProvider
+        implements Provider<LifecycleMapping>
+{
+    private static final String[] BINDINGS =
+            {
+                    "process-resources", "org.apache.maven.plugins:maven-resources-plugin:3.2.0:resources",
+                    "compile", "org.apache.maven.plugins:maven-compiler-plugin:3.8.1:compile",
+                    "process-test-resources", "org.apache.maven.plugins:maven-resources-plugin:3.2.0:testResources",
+                    "test-compile", "org.apache.maven.plugins:maven-compiler-plugin:3.8.1:testCompile",
+                    "test", "org.apache.maven.plugins:maven-surefire-plugin:3.0.0-M5:test",
+                    "package", "org.apache.maven.plugins:maven-rar-plugin:" + PluginInfo.get().getVersion() + ":rar",
+                    "install", "org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install",
+                    "deploy", "org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M1:deploy"
+            };
+
+    private final Lifecycle defaultLifecycle;
+
+    private final LifecycleMapping lifecycleMapping;
+
+    public RarLifecycleMappingProvider()
+    {
+        HashMap<String, String> bindings = new HashMap<>();
+        for ( int i = 0; i < BINDINGS.length; i = i + 2 )
+        {
+            bindings.put( BINDINGS[i], BINDINGS[i + 1] );
+        }
+        this.defaultLifecycle = new Lifecycle();
+        this.defaultLifecycle.setId( "default" );
+        this.defaultLifecycle.setPhases( bindings );
+
+        this.lifecycleMapping = new LifecycleMapping()
+        {
+            @Override
+            public Map<String, Lifecycle> getLifecycles()
+            {
+                return Collections.singletonMap( "default", defaultLifecycle );
+            }
+
+            @Override
+            public List<String> getOptionalMojos( String lifecycle )
+            {
+                return null;
+            }
+
+            @Override
+            public Map<String, String> getPhases( String lifecycle )
+            {
+                if ( "default".equals( lifecycle ) )
+                {
+                    return defaultLifecycle.getPhases();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        };
+
+    }
+
+    @Override
+    public LifecycleMapping get()
+    {
+        return lifecycleMapping;
+    }
+}
