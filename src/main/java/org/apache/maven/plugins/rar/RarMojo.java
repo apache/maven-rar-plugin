50560922ef277f4a75a94a74bc4d5f194fca9c53
Elliotte Rusty Harold <elharo@ibiblio.org>
2025-11-10 15:39:12 -0600

    Follow Oracle Javadoc conventions

--- src/main/java/org/apache/maven/plugins/rar/RarMojo.java
+++ src/main/java/org/apache/maven/plugins/rar/RarMojo.java
@@ -16,26 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.maven.plugins.rar;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+package org.apache.maven.plugins.rar;
 
 import javax.inject.Inject;
 
@@ -145,7 +127,7 @@ public class RarMojo extends AbstractMojo {
     private MavenArchiveConfiguration archive = new MavenArchiveConfiguration();
 
     /**
-     * allow filtering of link{rarSourceDirectory}
+     * Allow filtering of @link{rarSourceDirectory}.
      *
      * @since 2.3
      */
@@ -198,7 +180,7 @@ public class RarMojo extends AbstractMojo {
     protected boolean includeEmptyDirs;
 
     /**
-     * stop searching endToken at the end of line
+     * Stop searching endToken at the end of line.
      *
      * @since 2.3
      */
@@ -262,7 +244,7 @@ public class RarMojo extends AbstractMojo {
     protected List<String> nonFilteredFileExtensions;
 
     /**
-     * extra resource to include in rar archive
+     * Extra resource to include in rar archive.
      *
      * @since 2.3
      */
@@ -330,7 +312,9 @@ public class RarMojo extends AbstractMojo {
         this.projectHelper = projectHelper;
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     public void execute() throws MojoExecutionException {
 
         if (skip) {
@@ -449,7 +433,7 @@ public class RarMojo extends AbstractMojo {
     }
 
     /**
-     * @return The buildDir.
+     * @return the buildDir
      */
     protected File getBuildDir() {
         if (buildDir == null) {
@@ -459,10 +443,10 @@ public class RarMojo extends AbstractMojo {
     }
 
     /**
-     * @param basedir The basedir.
-     * @param finalName The finalName.
-     * @param classifier The classifier.
-     * @return the resulting file which contains classifier.
+     * @param basedir the base directory
+     * @param finalName the final name
+     * @param classifier the classifier
+     * @return the resulting file which contains classifier
      */
     protected static File getRarFile(File basedir, String finalName, String classifier) {
         if (classifier == null) {
