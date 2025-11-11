81c67574b5d015aa7e9dce728e60c3f6fd47e5d2
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2025-11-11 13:45:19 -0600

    Replace deprecated getPhases method (#129)
    * Replace deprecated method
    
    * Map.Entry

--- src/main/java/org/apache/maven/plugins/rar/internal/RarLifecycleMappingProvider.java
+++ src/main/java/org/apache/maven/plugins/rar/internal/RarLifecycleMappingProvider.java
@@ -24,6 +24,7 @@ import javax.inject.Singleton;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -79,7 +80,13 @@ public final class RarLifecycleMappingProvider implements Provider<LifecycleMapp
             @Override
             public Map<String, String> getPhases(String lifecycle) {
                 if ("default".equals(lifecycle)) {
-                    return defaultLifecycle.getPhases();
+                    Map<String, LifecyclePhase> lifecyclePhases = defaultLifecycle.getLifecyclePhases();
+                    Map<String, String> phases = new LinkedHashMap<>();
+                    for (Map.Entry<String, LifecyclePhase> entry : lifecyclePhases.entrySet()) {
+                        phases.put(entry.getKey(), entry.getValue().toString());
+                    }
+                    return phases;
+
                 } else {
                     return null;
                 }
